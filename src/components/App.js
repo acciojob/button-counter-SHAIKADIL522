717406b4d329c47cb31269b03d79d1a600688e3c
ADIL SHAIK <adilshaik9112@gmail.com>
2026-04-02 19:21:07 +0530

    code updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './../styles/App.css';
 
 const App = () => {
@@ -10,7 +10,6 @@ const App = () => {
 
   return (
     <div>
-      {/* Do not remove the main div */}
       <p>Button clicked {count} times</p>
       <button onClick={handleClick}>Click me</button>
     </div>
