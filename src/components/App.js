0cad84150664ef2f587bb590574f422d488ed240
ADIL SHAIK <adilshaik9112@gmail.com>
2026-04-02 11:20:10 +0530

    fix: button counter

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,12 @@ import './../styles/App.css';
 const App = () => {
   const [count, setCount] = useState(0);
 
-  const handleClick = () => {
-    setCount(count + 1);
-  };
-
   return (
     <div>
-      {/* Do not remove the main div */}
       <p>Button clicked {count} times</p>
-      <button onClick={handleClick}>Click me</button>
+      <button onClick={() => setCount(prev => prev + 1)}>
+        Click me
+      </button>
     </div>
   );
 };
